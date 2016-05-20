b83e68fe6836d34dac0cdd3f48cdd00036760b2c
EpicWolverine <epicwolverine@me.com>
2016-05-20 15:17:58 -0400

    Sound routing and direct link copying

--- js/controllers/SoundController.js
+++ js/controllers/SoundController.js
@@ -1,12 +1,30 @@
 app.controller('SoundController', ['$scope', 'Page', 'sounds', '$routeParams', '$location', function($scope, Page, sounds, $routeParams, $location) {
      sounds.success(function(data) {
         var redirect = true;
-        for(var index in data.cats){
-            if(data.cats[index].name.toLowerCase() == $routeParams.cat.toLowerCase()){
-                $scope.catDetail = data.cats[index];
-                redirect = false;
-                Page.setTitle($scope.catDetail.name + " - Parrot");
+        var looping = true;
+        for(var cat in data.cats){
+            for(var sound in data.cats[cat].sounds){
+                if(data.cats[cat].sounds[sound].src.toLowerCase() == $routeParams.filename.toLowerCase()){
+                    $scope.soundDetail = data.cats[cat].sounds[sound];
+                    redirect = false;
+                    looping = false;
+                    Page.setTitle($scope.soundDetail.text + " - Parrot");
+                    break;
+                }
             }
+            for(var subcat in data.cats[cat].subcats){
+                for(var sound in data.cats[cat].subcats[subcat].sounds){
+                    if(data.cats[cat].subcats[subcat].sounds[sound].src.toLowerCase() == $routeParams.filename.toLowerCase()){
+                        $scope.soundDetail = data.cats[cat].subcats[subcat].sounds[sound];
+                        redirect = false;
+                        looping = false;
+                        Page.setTitle($scope.soundDetail.text + " - Parrot");
+                        break;
+                    }
+                }
+                if(!looping) break;
+            }
+            if(!looping) break;
         }
         if(redirect){
             $location.path("/").replace();
